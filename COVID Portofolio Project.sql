3e0ffd241cb17f6d64abf24b3650cd9d553e6225
Manos Psarakis <89297370+ManosPsarakis@users.noreply.github.com>
2021-09-07 12:54:25 +0300

    Update COVID Portofolio Project.sql

--- COVID Portofolio Project.sql
+++ COVID Portofolio Project.sql
@@ -1,3 +1,6 @@
+--Covid 19 Data Exploration 
+--Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
 Select * 
 From [Portofolio Project.1]..CovidDeaths
 Order by 3,4
